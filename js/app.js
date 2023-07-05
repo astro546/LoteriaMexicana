a01b97931968cbef44fe420f9da86702528267af
Emiliano <oloroco1@hotmail.com>
2023-07-04 19:17:09 -0600

    Pagina de seleccion de juego

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 import { cartas } from './loteria.js';
+
+// Variables de la pagina de seleccion de variables
+let gameMode;
+const modes = document.querySelectorAll('.game-mode');
+
+// Variables de la pagina de juego
 const boardBoxes = Array.from(document.querySelectorAll('.board-box'));
 const currentCard = document.querySelector('#current-card-display');
 const progressBar = document.querySelector('#progress-bar');
 const imgCurrentCard = document.createElement('IMG');
 
+// Cuando se selecciona un modo, este se guarda en gameMode antes de que se cambie de pagina
+const selectGameMode = new Promise((resolve) => {
+  document.addEventListener('DOMContentLoaded', () => {
+    if (modes) {
+      modes.forEach((mode) => {
+        mode.addEventListener('click', (e) => {
+          gameMode = e.target.id;
+          resolve(gameMode);
+          window.location.href = 'index.html';
+        });
+      });
+    }
+  });
+});
+
+selectGameMode.then((gameMode) => console.log(gameMode));
 let i = 0;
 boardBoxes.forEach((box) => {
   const imgCard = document.createElement('IMG');
@@ -17,17 +39,6 @@ imgCurrentCard.src = cartas[0].img;
 imgCurrentCard.alt = cartas[0].nombre;
 currentCard.appendChild(imgCurrentCard);
 
-function timer() {
-  let progress = 0;
-  const interval = setInterval(() => {
-    progress += 10;
-    progressBar.style.width = `${progress}%`;
-    if (progress >= 100) {
-      progressBar.style.width = `0%`;
-    }
-  }, 1000);
-}
+selectGameMode.then((gameMode) => console.log(gameMode));
 
-// timer();
-
-progressBar.style.width = '100%';
+progressBar.style.width = '0%';
