d020b75ad872a88f7ffb1f782d96cf3a212b2693
Emiliano <oloroco1@hotmail.com>
2023-07-05 18:00:29 -0600

    Barajeo de cartas y generacion de cartas

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ const currentCard = document.querySelector('#current-card-display');
 const progressBar = document.querySelector('#progress-bar');
 const imgCurrentCard = document.createElement('IMG');
 
+// Variables de la logica del juego
+const mainDeck = [...cartas];
+const playerBoards = [];
+
 // Cuando se selecciona un modo, este se guarda en gameMode antes de que se cambie de pagina
 const selectGameMode = new Promise((resolve) => {
   document.addEventListener('DOMContentLoaded', () => {
@@ -18,7 +22,7 @@ const selectGameMode = new Promise((resolve) => {
         mode.addEventListener('click', (e) => {
           gameMode = e.target.id;
           resolve(gameMode);
-          window.location.href = 'index.html';
+          window.location.href = 'game.html';
         });
       });
     }
@@ -48,21 +52,176 @@ function initialTimer() {
   }, 900);
 }
 
-selectGameMode.then((gameMode) => console.log(gameMode));
-let i = 0;
-boardBoxes.forEach((box) => {
-  const imgCard = document.createElement('IMG');
-  imgCard.src = cartas[i].img;
-  imgCard.alt = cartas[i].nombre;
-  box.appendChild(imgCard);
-  i++;
-});
+function genIndex(boards) {
+  // Parametros de la formula
+  let s = Math.floor(Math.random() * 53);
+  const a = 47;
+  const c = 35;
+  const m = 59;
 
-imgCurrentCard.src = cartas[0].img;
-imgCurrentCard.alt = cartas[0].nombre;
-currentCard.appendChild(imgCurrentCard);
+  // Evita que se genere una semilla igual a otro tablero
+  if (Array.isArray(boards) && boards.includes(s)) {
+    while (boards.includes(s)) {
+      s = Math.floor(Math.random() * 53);
+    }
+  }
+  const x = [s];
 
-selectGameMode.then((gameMode) => console.log(gameMode));
+  // Genera la sucesion de numeros aleatorios
+  const limit = boards ? 15 : 53;
+  for (let i = 0; i < limit; i++) {
+    x.push((a * x[i] + c) % m);
+  }
 
-progressBar.style.width = '0%';
+  // Verifica si hay numeros entre 0 y 53 que no estan en la sucesion
+  const skippedNums = [];
+  if (!boards) {
+    for (let i = 0; i < 54; i++) {
+      if (!x.includes(i)) {
+        skippedNums.push(i);
+      }
+    }
+
+    // Reemplaza los numeros mayores a 53 por los numeros que no estaban en la sucesion
+    let replacedNum = 54;
+    let indexReplacedNum;
+    let remainingNumber;
+    for (let skipedNum of skippedNums) {
+      indexReplacedNum = x.indexOf(replacedNum);
+      if (indexReplacedNum !== -1) {
+        x[indexReplacedNum] = skipedNum;
+      } else {
+        remainingNumber = skipedNum;
+      }
+      replacedNum++;
+    }
+
+    // Si un numero mayor a 53 tampoco estaba en la sucesion, entonces el valor final de replacedNum sera reemplazado
+    if (remainingNumber) {
+      indexReplacedNum = x.indexOf(replacedNum);
+      x[indexReplacedNum] = remainingNumber;
+    }
+  } else {
+    // Reemplaza los numeros mayores a 53 para los tableros de los jugadores
+    let i = 0;
+    for (let num of x) {
+      if (num >= 54) {
+        let newNum = Math.floor(Math.random() * 53);
+        if (x.includes(newNum)) {
+          while (x.includes(newNum)) {
+            newNum = Math.floor(Math.random() * 53);
+          }
+        }
+        x[i] = newNum;
+      }
+      i++;
+    }
+  }
+
+  // Si la sucesion solo genera puros 39, entonces vuelve a llamar a la funcion
+  const set = new Set(x);
+  if (set.size === 1) {
+    let newX = genIndex(boards);
+    return newX;
+  }
+
+  return x;
+}
+
+// Intercambia las cartas
+function swap(index1, index2) {
+  const temp = mainDeck[index1];
+  mainDeck[index1] = mainDeck[index2];
+  mainDeck[index2] = temp;
+}
+
+// Barajea las cartas del mazo principal y genera los tableros para los jugadores
+function shuffleCards() {
+  // Barajea el mazo de cartas principal
+  const randomIndexesDeck = genIndex();
+  for (let i = 0; i < 54; i++) {
+    swap(i, randomIndexesDeck[i]);
+  }
+
+  // Genera los tableros para los jugadores
+  let randomIndexesBoard = [];
+  let board = [];
+  const initialCards = [];
+  for (let i = 0; i < 5; i++) {
+    if (i > 0) {
+      initialCards.push(playerBoards[i - 1][0].num - 1);
+    }
+    randomIndexesBoard = genIndex(initialCards);
+    for (let randomIndex of randomIndexesBoard) {
+      board.push(cartas[randomIndex]);
+    }
+    playerBoards.push(board);
+    board = [];
+  }
+}
+
+function showCards(modeDisplayHTML) {
+  // Muestra el tablero del jugador humano
+  let i = 0;
+  boardBoxes.forEach((box) => {
+    const imgCard = document.createElement('IMG');
+    imgCard.src = playerBoards[0][i].img;
+    imgCard.alt = playerBoards[0][i].nombre;
+    box.appendChild(imgCard);
+    i++;
+  });
+
+  // Muestra la primera carta del mazo
+  imgCurrentCard.src = mainDeck[0].img;
+  imgCurrentCard.alt = mainDeck[0].nombre;
+  currentCard.appendChild(imgCurrentCard);
+
+  const modeDisplay = document.querySelector('#mode-display');
+  modeDisplay.innerHTML += modeDisplayHTML;
+}
+
+function showMode(mode) {
+  // Muestra el modo de juego
+  const gMode = mode.slice(0, 6);
+  let HTML;
+  console.log(gMode);
+  switch (gMode) {
+    case 'mode-1':
+      HTML = `
+      <img
+          class="game-mode-icon"
+          id="mode-1-icon"
+          src="/img/icons/Loteria-Columna.png"
+          alt="row-icon"
+        />
+      `;
+      break;
+    case 'mode-2':
+      break;
+    case 'mode-3':
+      break;
+    case 'mode-4':
+      break;
+    case 'mode-5':
+      break;
+    default:
+      break;
+  }
+  return HTML;
+}
+
+function startGame() {
+  let modeDisplayHTML;
+  selectGameMode.then((mode) => {
+    modeDisplayHTML = showMode(mode);
+    console.log(modeDisplayHTML);
+    // console.log(mode);
+  });
+
+  // shuffleCards();
+  // showCards(modeDisplayHTML);
+}
+
+// progressBar.style.width = '0%';
 initialTimer();
+startGame();
