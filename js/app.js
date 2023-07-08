977cd1e6c325bd363851cd336ef6bd4ba39b4d5a
Emiliano <oloroco1@hotmail.com>
2023-07-08 00:34:24 -0600

    Alerta de ganador y bugs solucionados de funciones de ganador

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ const mainDeck = [...cartas];
 const playerBoards = [];
 let winnerFunc;
 let modeSet;
+let gameOver = false;
 
 // Cuando se selecciona un modo, este se guarda en gameMode antes de que se cambie de pagina
 const selectGameMode = new Promise((resolve) => {
@@ -38,7 +39,7 @@ function initialTimer() {
   let timer = 3;
   const timerDiv = document.createElement('DIV');
   const timerNumber = document.createElement('h2');
-  timerDiv.id = 'alert';
+  timerDiv.id = 'alert-timer';
   timerNumber.id = 'timer-number';
   timerNumber.textContent = `${timer}`;
   timerDiv.appendChild(timerNumber);
@@ -52,12 +53,14 @@ function initialTimer() {
       timerDiv.style.opacity = '0';
       setTimeout(() => {
         timerDiv.remove();
+        progressBar.style.animation = 'progress-animation 3s infinite';
       }, 100);
       clearInterval(initialTimerInterval);
     }
   }, 900);
 }
 
+// Genera los numeros aleatorios
 function genIndex(boards) {
   // Parametros de la formula
   let s = Math.floor(Math.random() * 53);
@@ -166,10 +169,15 @@ function shuffleCards() {
   }
 }
 
+// Muestra las cartas y tableros
 function showCards() {
   // Muestra el tablero del jugador humano
   let i = 0;
   boardBoxes.forEach((box) => {
+    const lastImg = box.querySelector('img');
+    if (lastImg) {
+      lastImg.remove();
+    }
     const imgCard = document.createElement('IMG');
     imgCard.src = playerBoards[0][i].img;
     imgCard.alt = playerBoards[0][i].nombre;
@@ -188,6 +196,14 @@ function showCards() {
 function showMode() {
   // Muestra el modo de juego
   const modeDisplay = document.querySelector('#mode-display');
+  const gameModeIcon = document.querySelector('.game-mode-icon');
+  const gameModeTitleLast = document.querySelector('.game-mode-title');
+
+  if (gameModeIcon && gameModeTitleLast) {
+    gameModeIcon.remove();
+    gameModeTitleLast.remove();
+  }
+
   let id;
   let nomImg;
   let altImg;
@@ -252,7 +268,7 @@ function setBeanHuman(e) {
     bean.classList.add('bean');
     box.parentNode.classList.add('marked');
     box.insertAdjacentElement('afterend', bean);
-  } else {
+  } else if (!box.parentNode.classList.contains('marked')) {
     const alert = document.createElement('div');
     alert.classList.add('block-alert');
     alert.innerHTML = '<h4>CASILLA INCORRECTA</h4>';
@@ -285,11 +301,13 @@ function setBeanPC(boardPC, playerID) {
 function winnerRowColumnCorners(boxes, index, gameMode) {
   let boxesSet;
   let winner;
-  for (let j = 1; j <= 3; j++) {
-    boxesSet = Array.from(boxes.querySelectorAll(`${gameMode}-${j}`));
-    winner = boxesSet.every((box) => box.classList.includes('marked'));
+  for (let j = 1; j <= 4; j++) {
+    boxesSet = Array.from(boxes.querySelectorAll(`.${gameMode}-${j}`));
+    winner = boxesSet.every((box) => box.classList.contains('marked'));
+    console.log(winner);
     if (winner) {
-      return index;
+      console.log(index);
+      return index !== 0 ? index : 'Humano';
     }
   }
   return false;
@@ -299,16 +317,16 @@ function winnerRowColumnCorners(boxes, index, gameMode) {
 function winnerInsideOutside(boxes, index) {
   let setBoxes;
   let winner;
-  setBoxes = Array.from(boxes.querySelectorAll('inside'));
-  winner = setBoxes.every((box) => box.classList.includes('marked'));
+  setBoxes = Array.from(boxes.querySelectorAll('.inside'));
+  winner = setBoxes.every((box) => box.classList.contains('marked'));
   if (winner) {
-    return index;
+    return index !== 0 ? index : 'Humano';
   }
 
-  setBoxes = Array.from(boxes.querySelectorAll('outside'));
-  winner = setBoxes.every((box) => box.classList.includes('marked'));
+  setBoxes = Array.from(boxes.querySelectorAll('.outside'));
+  winner = setBoxes.every((box) => box.classList.contains('marked'));
   if (winner) {
-    return index;
+    return index !== 0 ? index : 'Humano';
   }
   return false;
 }
@@ -318,9 +336,13 @@ function winnerAll(boxes, index) {
   let setBoxes;
   let winner;
   setBoxes = Array.from(boxes.childNodes);
-  winner = setBoxes.every((box) => box.classList.includes('marked'));
+  setBoxes = setBoxes.filter((box) => box.nodeType !== box.TEXT_NODE);
+  winner = setBoxes.every((box) => box.classList.contains('marked'));
+  if (index === 0) {
+    console.log(winner);
+  }
   if (winner) {
-    return index;
+    return index !== 0 ? index : 'Humano';
   }
   return false;
 }
@@ -366,6 +388,171 @@ function winner() {
   return false;
 }
 
+// Limpia todos los tableros
+function clearBoards() {
+  let playerBoard;
+  let setBoxes;
+  let bean;
+  for (let i = 0; i < 5; i++) {
+    playerBoard = document.querySelector(`#playerPC${i}`);
+    setBoxes = Array.from(playerBoard.childNodes);
+    setBoxes = setBoxes.filter((box) => box.nodeType !== box.TEXT_NODE);
+    for (let box of setBoxes) {
+      if (box.classList.contains('marked')) {
+        box.classList.remove('marked');
+        if (box.classList.contains('oponent-board-box')) {
+          box.style.backgroundColor = '#fcd188';
+        } else {
+          bean = box.querySelector('.bean');
+          bean.remove();
+        }
+      }
+    }
+  }
+}
+
+// Muestra al ganador en pantalla
+function showWinner(winner) {
+  const winnerAlert = document.createElement('div');
+  const winnerText = document.createElement('h3');
+  const alertButtons = document.createElement('div');
+  const volverBtn = document.createElement('a');
+  const modoBtn = document.createElement('a');
+
+  winnerAlert.id = 'alert-winner';
+  winnerText.id = 'winner-text';
+  alertButtons.id = 'alert-buttons';
+  volverBtn.classList.add('alert-button');
+  modoBtn.classList.add('alert-button');
+
+  winnerText.textContent = `EL JUGADOR ${winner} HA GANADO LA PARTIDA`;
+  volverBtn.textContent = 'VOLVER A JUGAR';
+  modoBtn.textContent = 'ESCOJER OTRO MODO DE JUEGO';
+
+  winnerAlert.appendChild(winnerText);
+  alertButtons.appendChild(volverBtn);
+  alertButtons.appendChild(modoBtn);
+  winnerAlert.appendChild(alertButtons);
+  document.body.appendChild(winnerAlert);
+
+  modoBtn.onclick = function () {
+    window.location.href = 'index.html';
+  };
+
+  volverBtn.onclick = function () {
+    clearBoards();
+    winnerAlert.remove();
+    initialTimer();
+    startGame();
+  };
+}
+
+// Bucle del juego
+function play() {
+  let mainDeckIndex = 0;
+
+  const interval = setInterval(() => {
+    imgCurrentCard.src = mainDeck[mainDeckIndex].img;
+    imgCurrentCard.alt = mainDeck[mainDeckIndex].nombre;
+
+    for (let i = 1; i < 5; i++) {
+      setBeanPC(playerBoards[i], i);
+    }
+
+    mainDeckIndex++;
+
+    gameOver = winner();
+    if (gameOver || mainDeckIndex >= 54) {
+      console.log(`El ganador es el jugador ${gameOver}`);
+      showWinner(gameOver);
+      progressBar.style.animation = 'none';
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+// function humanWins() {
+//   // let countersSets = 0;
+//   // console.log(countersSets);
+//   const countersSets = [];
+//   // console.log(countersSets, mainDeck);
+//   const filasIndexes = [
+//     [0, 1, 2, 3],
+//     [4, 5, 6, 7],
+//     [8, 9, 10, 11],
+//     [12, 13, 14, 15],
+//   ];
+//   const columnasIndexes = [
+//     [0, 4, 8, 12],
+//     [1, 5, 9, 13],
+//     [2, 6, 10, 14],
+//     [3, 7, 11, 15],
+//   ];
+//   const esquinasIndexes = [
+//     [0, 1, 4, 5],
+//     [2, 3, 6, 7],
+//     [8, 9, 12, 13],
+//     [10, 11, 14, 15],
+//   ];
+//   let containsCard;
+
+//   let win;
+
+//   let temp = [];
+//   for (let playerBoard of playerBoards) {
+//     for (let i = 0; i < 4; i++) {
+//       for (let box of esquinasIndexes[i]) {
+//         for (let j = 0; j < 54; j++) {
+//           // console.log(card);
+//           if (mainDeck[j].nombre === playerBoard[box].nombre) {
+//             temp.push(j);
+//           }
+//         }
+//       }
+//     }
+//     countersSets.push(temp);
+//     temp = [];
+//   }
+
+// console.log(countersSets);
+//   let reorganizedBoards = [];
+//   let reorganizedBoard = [];
+//   let rowBoard = [];
+//   console.log(reorganizedBoards, countersSets);
+
+//   for (let w = 0; w < 5; w++) {
+//     for (let k = 0; k < 4; k++) {
+//       for (let box of esquinasIndexes[k]) {
+//         rowBoard.push(countersSets[w][box]);
+//       }
+//       reorganizedBoard.push(rowBoard);
+//       rowBoard = [];
+//     }
+//     reorganizedBoards.push(reorganizedBoard);
+//     reorganizedBoard = [];
+//   }
+
+//   let maxNumbersBoardsRows = [];
+//   let maxNumbersBoardRow = [];
+//   for (let board of reorganizedBoards) {
+//     for (let x = 0; x < 4; x++) {
+//       maxNumbersBoardRow.push(Math.max(...board[x]));
+//     }
+//     maxNumbersBoardsRows.push(maxNumbersBoardRow);
+//     maxNumbersBoardRow = [];
+//   }
+//   console.log(maxNumbersBoardsRows);
+
+//   const minMaxNumbersBoards = [];
+//   for (let maxNumbersList of maxNumbersBoardsRows) {
+//     minMaxNumbersBoards.push(Math.min(...maxNumbersList));
+//   }
+//   console.log(minMaxNumbersBoards);
+
+//   win = minMaxNumbersBoards.indexOf(Math.min(...minMaxNumbersBoards));
+//   console.log(`El jugador ${win} ganara la partida`);
+// }
+
 // Inicia el juego
 function startGame() {
   selectGameMode.then((mode) => {
@@ -377,17 +564,15 @@ function startGame() {
   showMode(gameMode);
   shuffleCards();
   showCards();
-
-  for (let i = 1; i < 5; i++) {
-    setBeanPC(playerBoards[i], i);
-  }
+  // humanWins();
 
   boardBoxes.forEach((boardBox) => {
     boardBox.addEventListener('click', setBeanHuman);
   });
+
+  play();
 }
 
-// progressBar.style.width = '0%';
 if (document.querySelector('#game-layout')) {
   initialTimer();
   startGame();
